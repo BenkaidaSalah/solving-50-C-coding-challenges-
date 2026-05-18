8275b906d6b5757238b381629ac6112c84f187ba
Ben Kaida Salah Eddine <benkadataza16@gmail.com>
2026-05-18 20:39:00 +0100

    Add a New Porblem 19 Minimum and Maximum Number Print In Matrix Solution.

---
+++ problems/Problem19/PrintMimimumandMaximumNumberInMatrix.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int RandomNumber(int From, int To)
+{
+    // Function to generate a random number
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+
+void FillMatrixWithRandomNumbers(int arr[3][3], short Rows, short Cols)
+{
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            arr[i][j] = RandomNumber(1, 10);
+        }
+    }
+}
+
+void PrintMatrix(int arr[3][3], short Rows, short Cols)
+{
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            printf(" %0*d ", 2, arr[i][j]);
+            // cout << setw(3) << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+int GetMinNumberInMatrix(int Matrix1[3][3],short Rows, short Cols)
+{
+	int MinNumber = Matrix1[0][0];
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            if (Matrix1[i][j] < MinNumber)
+            {
+                MinNumber = Matrix1[i][j];
+			}
+            
+        }
+    }
+	return MinNumber;
+}
+int GetMaxNumberInMatrix(int Matrix1[3][3], short Rows, short Cols)
+{
+    int MaxNumber = Matrix1[0][0];
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            if (Matrix1[i][j] > MaxNumber)
+            {
+                MaxNumber = Matrix1[i][j];
+            }
+
+        }
+    }
+    return MaxNumber;
+}
+void PrintMinNumberInMatrix(int Matrix1[3][3], short Rows, short Cols)
+{
+    int MinNumber = GetMinNumberInMatrix(Matrix1, Rows, Cols);
+    cout << "\nThe Minimum Number in the Matrix is: " << MinNumber << "\n";
+}
+void PrintMaximumNumberInMatrix(int Matrix1[3][3], short Rows, short Cols)
+{
+    int MaxNumber = GetMaxNumberInMatrix(Matrix1, Rows, Cols);
+    cout << "\nThe Maximum Number in the Matrix is: " << MaxNumber << "\n";
+}
+int main()
+{
+    // Seeds the random number generator in C++, called only once
+    srand((unsigned)time(NULL));
+
+    int Matrix1[3][3], Matrix2[3][3];
+
+    FillMatrixWithRandomNumbers(Matrix1, 3, 3);
+    cout << "\nMatrix1:\n";
+    PrintMatrix(Matrix1, 3, 3);
+    PrintMinNumberInMatrix(Matrix1, 3, 3);
+    PrintMaximumNumberInMatrix(Matrix1, 3, 3);
+    return 0;
+}
