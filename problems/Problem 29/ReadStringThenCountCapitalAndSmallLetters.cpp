7da7af531319aadbf52b44e183e893de5c7162b5
Ben Kaida Salah Eddine <benkadataza16@gmail.com>
2026-05-24 18:20:40 +0100

    Add Problem 29 With the Solution .

---
+++ problems/Problem 29/ReadStringThenCountCapitalAndSmallLetters.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <iostream>
+using namespace std;
+string ReadString()
+{
+	string S1;
+	cout << "Please Enter Your String?\n";
+	getline(cin, S1);
+	return S1;
+}
+enum enWhatToCount { SmallLetters = 0, CapitalLetters = 1, All = 3 };
+short CountLetters(string S1, enWhatToCount
+	WhatToCount = enWhatToCount::All)
+{
+	if (WhatToCount == enWhatToCount::All)
+	{
+		return S1.length();
+	}
+	short Counter = 0;
+	for (short i = 0; i < S1.length(); i++)
+	{
+		if (WhatToCount == enWhatToCount::CapitalLetters &&
+			isupper(S1[i]))
+			Counter++;
+		if (WhatToCount == enWhatToCount::SmallLetters &&
+			islower(S1[i]))
+			Counter++;
+	}
+	return Counter;
+}
+int main()
+{
+	string S1 = ReadString();
+	cout << "\nString Length = " << CountLetters(S1);
+	cout << "\nCapital Letters Count= " << CountLetters(S1,
+		enWhatToCount::CapitalLetters);
+	cout << "\nSmall Letters Count= " <<
+		CountLetters(S1, enWhatToCount::SmallLetters);
+}
